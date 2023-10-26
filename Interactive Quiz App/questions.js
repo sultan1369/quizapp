a0feb6e134f90e5234a2671dd5d825668f750275
sultan <sultanbasha1369@gmail.com>
2023-10-26 15:32:53 +0530

    html

---
+++ Interactive Quiz App/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What Does HTML Stands For?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocesser",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does PHP stands for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the purpose of the meta tag in HTML, and how is it commonly used?",
+        answer: "meta charset=UTF-8",
+        options: [
+            "meta Charset=UTF-8",
+            "meta charSet=UTF-8",
+            "meta charset=UTF-8",
+            "meta charset=UTF-6"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What Does CSS Stands For?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colourful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Does SQL Stands For?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+];
